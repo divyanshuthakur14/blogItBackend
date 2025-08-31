2f9532ee0a3788723d1a3eac0521e0217979859b
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2025-09-01 01:12:56 +0530

    containerized

---
+++ src/main/java/com/blogit/blogit_backend/BlogitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blogit.blogit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogit/blogit_backend/BlogitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogit.blogit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
